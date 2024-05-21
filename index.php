75cd03d6c7b72a56069492ad7f05a6280667da0c
lostorres2000 <tdamian453@gmail.com>
2024-05-21 18:40:30 -0300

    css de login

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="contenedor-login">
+        <img src="logo.jpg" alt="Logo" class="logo">
+        <img src="fotousuario.jpg" alt="Foto de Usuario" class="foto-usuario">
+        <input type="text" placeholder="Nombre de Usuario" required>
+        <input type="password" placeholder="Contraseña" required>
+        <button class="boton-iniciar">Iniciar Sesión</button>
+        <button class="boton-olvidado">Me olvidé mi contraseña</button>
+    </div>
+</body>
+</html>
