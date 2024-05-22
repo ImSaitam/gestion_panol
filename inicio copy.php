0e59c1e205cea259c0ded04dd08836da527170ba
lugengar <luciano.barbinii@gmail.com>
2024-05-22 16:57:12 -0300

    a

--- inicio copy.php
+++ inicio copy.php
@@ -8,12 +8,14 @@
 <body>
     <div id="pagina">
         <div id="header">
-            <div class="logo"></div>
-            <div class="usuario"></div>
+            <button class="logo"></button>
+            <button class="usuario"></button>
         </div>
         <div id="subheader"></div>
         <div id="contenido"></div>
-        <div id="footer"></div>
+        <div id="footer">
+        <button class="usuario"></button>
+        </div>
     </div>
     
 </body>
@@ -28,25 +30,31 @@
     width: 100%;
     display:grid;
     grid-template:
-    "HEA HEA HEA"6vh
+    "HEA HEA HEA"7vh
     "SHEA SHEA SHEA"20vh
     "CON CON CON"auto
-    "FOO FOO FOO"6vh/
+    "FOO FOO FOO"7vh/
     auto auto auto;
     background-color:#202427;
 }
 #header{
+    padding-left:2vh;
+    padding-right:2vh;
     display: grid;
     grid-template:
     ". . ." auto
-    "LOGO . USUARIO"4vh
+    "LOGO . USUARIO"5vh
     ". . ." auto/
-    4vh auto 4vh;
+    5vh auto 5vh;
     grid-area: HEA;
     background-color:#202427;
     box-shadow: 1vh 0.5vh 3vh 0vh rgba(61, 58, 230, 0.1);
 }
 #subheader{
+    display: grid;
+    grid-template:
+    ". " auto/
+    5vh auto 5vh;
     grid-area: SHEA;
     background-color:transparent;
 }
@@ -60,9 +68,31 @@
     grid-area: FOO;
     background-color:white;
     box-shadow: 1vh -0.5vh 3vh 0vh rgba(208, 207, 238, 1);
+    display: grid;
+    grid-column: repeat(5vh);
+    gap: auto;
+}
+button{
+    background-color: transparent;
+    border: none;
 }
 .logo{
-    background-image: URL(imagenes/);
+    background-image: URL(imagenes/logo.png);
+    background-size: contain;
+    background-repeat:no-repeat;
+    grid-area: LOGO;
+}
+.usuario{
+    background-image: URL(imagenes/user.png);
+    background-size: contain;
+    background-repeat:no-repeat;
+    grid-area: USUARIO;
+}
+.botonlogo{
+    background-image: URL(imagenes/botonlogo.png);
+    background-size: contain;
+    background-repeat:no-repeat;
+    grid-area: USUARIO;
 }
 </style>
 
