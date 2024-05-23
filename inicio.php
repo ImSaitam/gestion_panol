30b458fe75af6eb95675ad10becb8e4b72e7751c
lugengar <luciano.barbinii@gmail.com>
2024-05-23 00:49:14 -0300

    a

--- inicio.php
+++ inicio.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['id_usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de Inicio</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div id="pagina">
