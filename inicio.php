1d9fb792928e29788f5288721b92c46e6c2499d1
angeles123o <angeleslinaresrios@gmail.com>
2024-06-04 17:57:49 -0300

    a

--- inicio.php
+++ inicio.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['id_usuario'])) {
 include './codigophp/conexionbs.php'
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
