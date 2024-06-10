ecb845496ea6307e33fab083d0ae8789ba7e63a9
imsaitam <matutedesanto@gmail.com>
2024-06-10 00:01:38 -0300

    barra de busqueda funcional

--- inicio.php
+++ inicio.php
@@ -6,7 +6,8 @@ include './codigophp/conexionbs.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="es"> 
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,24 +17,27 @@ include './codigophp/conexionbs.php';
     <link rel="stylesheet" href="estiloscss/animaciones.css">
 
 </head>
+
 <body>
-    
+
     <div id="pagina">
         <div id="header">
-            <a href="inicio.php" class="logo imagen" ></a>
+            <a href="inicio.php" class="logo imagen"></a>
 
-            <button  class="usuario imagen" id="user"></button>
+            <button class="usuario imagen" id="user"></button>
 
 
         </div>
         <div id="subheader">
             <h1>Bienvenido <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit.</p>
         </div>
         <div id="contenido">
-            <form class="barra" method="post">
-                <input type="submit" class="equis" value=""></button>
-                <input type="text" placeholder="Buscar..">
+            <form class="barra" method="get" id="search-form">
+                <input type="submit" class="equis" value="">
+                <input type="text" id="search-input" name="search" placeholder="Buscar..">
+                <button type="submit"><img src="./estiloscss/imagenes/lupa.png" class="boton-search"></button>
                 <div></div>
             </form>
             <div class="contenido2">
@@ -41,31 +45,47 @@ include './codigophp/conexionbs.php';
                     <h1>HERRAMIENTAS</h1>
                     <div class="scroll-x">
                         <div class="conscroll-x">
+                            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+                                <input type="search" name="search" placeholder="Buscar herramienta" autocomplete="off">
+                            </form>
                             <?php
+                            if (isset($_GET['search'])) {
+                                $search = $_GET['search'];
+                                $sql = "SELECT nombre, descripcion FROM categoria WHERE nombre LIKE '%$search%'";
+                                $result = $conn->query($sql);
+                                if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo '<button class="cubo"> <h1>' . $row["nombre"] . '</h1> <p>Descripción: ' . $row["descripcion"] . '</p></button>';
+                                    }
+                                } else {
+                                    echo '<p>No se encontraron resultados</p>';
+                                }
+                            } else {
                                 $sql = "SELECT nombre, descripcion FROM categoria";
                                 $result = $conn->query($sql);
                                 if ($result->num_rows > 0) {
-                                    while($row = $result->fetch_assoc()) {
-                                        echo '<button class="cubo"> <h1>'.$row["nombre"].'</h1> <p>Descripción: '.$row["descripcion"].'</p></button>';
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo '<button class="cubo"> <h1>' . $row["nombre"] . '</h1> <p>Descripción: ' . $row["descripcion"] . '</p></button>';
                                     }
+                                } else {
+                                    echo '<p>No se encontraron resultados</p>';
                                 }
-                            ?> 
-                           
-                            
+                            }
+                            ?>
                         </div>
                     </div>
                     <h1>PAÑOLEROS</h1>
-                    <div class="scroll-x"style="height: 28vh;">
-                        <div class="conscroll-x" > 
+                    <div class="scroll-x" style="height: 28vh;">
+                        <div class="conscroll-x">
                             <?php
                             $sql = "SELECT nombre_completo, horario FROM usuarios WHERE cargo = 'panolero' ";
                             $result = $conn->query($sql);
                             if ($result->num_rows > 0) {
-                                while($row = $result->fetch_assoc()) {
-                                    echo "<div class='rectangulo'><h1>".$row["nombre_completo"]."<p>".$row["horario"]."</p></div>";
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "<div class='rectangulo'><h1>" . $row["nombre_completo"] . "<p>" . $row["horario"] . "</p></div>";
+                                }
                             }
-                            }
-                            ?>            
+                            ?>
                         </div>
                     </div>
                 </div>
@@ -77,26 +97,27 @@ include './codigophp/conexionbs.php';
             <a href="reportes.php" class="alerta imagen derecha">Reportes</a>
         </div>
         <div id="sombra2" class="sombra">
-        <div class="contenidosombra">
-        <button class="barra" id="opcionequis2">
-                <div class="equis" ></div>
+            <div class="contenidosombra">
+                <button class="barra" id="opcionequis2">
+                    <div class="equis"></div>
                     <div>Volver</div>
                     <div></div>
-            </button>
-            <div class="contenido2">
-                <div class="con3" id="inicio">
-                    <div class="scroll-y" style="height: 100%; padding-top:2vh;">
-                        <div class="conscroll-y">
+                </button>
+                <div class="contenido2">
+                    <div class="con3" id="inicio">
+                        <div class="scroll-y" style="height: 100%; padding-top:2vh;">
+                            <div class="conscroll-y">
                                 <a href="codigophp/cerrarsesion.php" class="flecha imagen boton">Cerrar sesión</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-   
+
 </body>
+
 </html>
-<?php $conn->close();?>
+<?php $conn->close(); ?>
 <script src="./codigojs/sombra2.js"></script>
