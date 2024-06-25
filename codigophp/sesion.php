48c51e68a96aa5d28cba8de86a0d0fc80ffa06dc
lugengar <luciano.barbinii@gmail.com>
2024-06-25 20:42:58 -0300

    a

--- codigophp/sesion.php
+++ codigophp/sesion.php
@@ -4,4 +4,10 @@ if (!isset($_SESSION['id_usuario'])) {
     header("Location: ./index.php");
     exit;
 }
+
+function espanol($contenido){
+    if ($_SESSION['cargo'] == "panolero") {
+        echo $contenido;
+    }
+}
 ?>
