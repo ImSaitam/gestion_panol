c32eafa96153d3e0a578aeaa46728f8fa32b8b8a
lugengar <luciano.barbinii@gmail.com>
2024-07-02 19:10:08 -0300

    a

--- notificaciones.php
+++ notificaciones.php
@@ -32,29 +32,28 @@ include "./codigophp/conexionbs.php";
                 <h1>PEDIDOS DE HOY</h1>
                     <div class="scroll-y" style="height: 100%;">
                         <div class="conscroll-y" style="height: 100%;">
-                        <?php
-                            $sql = "SELECT pedidos.*, aulas.*, cursos.*
-                            FROM pedidos
-                            JOIN aulas ON pedidos.id_aula = aulas.id_aulas
-                            JOIN cursos ON pedidos.fk_curso = cursos.id
-                           
-                            ORDER BY pedidos.fecha_pedido DESC;
-                            ";  
-                            $result = $conn->query($sql);
-                            if ($result->num_rows > 0) {
-                                while($row = $result->fetch_assoc()) {
-                                    if (date('Y-m-d') == date('Y-m-d', strtotime($row["fecha_pedido"])) && $row["estado"] == "pendiente") {
-                                        echo '<div class="rectangulo4 verde"><h1>'.$row["fecha_pedido"].' '.$row["estado"].'</h1> <p>Aula: '.$row["nombre"].'<br>Curso:'.$row["curso"].' </p> <input type="hidden" name="id" id="id" value="'.$row["id_pedido"].'"><input type="hidden" name="estado" id="estado" value="'.$row["estado"].'"><input type="hidden" name="pedido" id="pedido" value="'.htmlspecialchars($row["pedido"],ENT_QUOTES, 'UTF-8').'"> <button class="imagen opcionesblanco tocar"></button></div>';
-                                    }else{
-                                        echo '<div class="rectangulo2"><h1>'.$row["fecha_pedido"].'</h1> <p>'.$row["id_aula"].' '.$row["estado"].'</p> <input type="hidden" name="id" id="id" value="'.$row["id_pedido"].'"><input type="hidden" name="estado" id="estado" value="'.$row["estado"].'"><input type="hidden" name="pedido" id="pedido" value="'.htmlspecialchars($row["pedido"],ENT_QUOTES, 'UTF-8').'"> <button class="imagen opciones tocar"></button></div>';
-                                    }
-                                }
-                            } else {
-                                echo "<h1>NO HAY PEDIDOS AUN</h1>";
-                            }
+                            <?php
+                                $sql = "SELECT pedidos.*, aulas.*, cursos.*
+                                FROM pedidos
+                                JOIN aulas ON pedidos.id_aula = aulas.id_aulas
+                                JOIN cursos ON pedidos.fk_curso = cursos.id
                             
-                            $conn->close();
-                        ?>          
+                                ORDER BY pedidos.fecha_pedido DESC;
+                                ";  
+                                $result = $conn->query($sql);
+                                if ($result->num_rows > 0) {
+                                    while($row = $result->fetch_assoc()) {
+                                        if (date('Y-m-d') == date('Y-m-d', strtotime($row["fecha_pedido"])) && $row["estado"] == "pendiente") {
+                                            echo '<div class="rectangulo4 verde"><h1>'.$row["fecha_pedido"].' '.$row["estado"].'</h1> <p>Aula: '.$row["nombre"].'<br>Curso:'.$row["curso"].' </p> <input type="hidden" name="id" id="id" value="'.$row["id_pedido"].'"><input type="hidden" name="estado" id="estado" value="'.$row["estado"].'"><input type="hidden" name="pedido" id="pedido" value="'.htmlspecialchars($row["pedido"],ENT_QUOTES, 'UTF-8').'"> <button class="imagen opcionesblanco tocar"></button></div>';
+                                        }else{
+                                            echo '<div class="rectangulo2"><h1>'.$row["fecha_pedido"].'</h1> <p>'.$row["id_aula"].' '.$row["estado"].'</p> <input type="hidden" name="id" id="id" value="'.$row["id_pedido"].'"><input type="hidden" name="estado" id="estado" value="'.$row["estado"].'"><input type="hidden" name="pedido" id="pedido" value="'.htmlspecialchars($row["pedido"],ENT_QUOTES, 'UTF-8').'"> <button class="imagen opciones tocar"></button></div>';
+                                        }
+                                    }
+                                } else {
+                                    echo "<h1>NO HAY PEDIDOS AUN</h1>";
+                                }
+                                $conn->close();
+                            ?>          
                         </div>
                     </div>
                 </div>
